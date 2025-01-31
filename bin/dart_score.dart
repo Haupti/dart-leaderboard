d8959e22c39593302134c006678305d49631639f
redstar <marwin.ack@googlemail.com>
2025-01-31 22:23:37 +0100

    initial commit

---
+++ bin/dart_score.dart
@@ -0,0 +1,5 @@
+import 'package:dart_score/dart_score.dart' as dart_score;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_score.calculate()}!');
+}
---
+++ test/dart_score_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_score/dart_score.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
